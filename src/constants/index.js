abeae2146a34dfb22c86b4d568cd6bb0390fea31
koni-hub <delgadoargie04z@gmail.com>
2025-09-19 21:42:55 +0800

    chore: change the profile

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 // -----------------
 // Hero Data
 import profile from "../assets/profile/profile.png";
-import profile_shades from "../assets/profile/profile-upate.png";
+import profile_shades from "../assets/profile/profile.png";
 
 export const heroData = {
   title: "Full Stack Developer",
